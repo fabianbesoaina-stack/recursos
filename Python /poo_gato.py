d536638fedac34903820e01f4b7e4813ffc5a7df
fabian besoain <fabianbesoaina@gmail.com>
2026-04-20 21:30:23 -0400

    Update poo_gato.py

--- Python /poo_gato.py
+++ Python /poo_gato.py
@@ -1,1 +1,15 @@
 
+class Gato:
+  color = "default"
+  def hablar(self):
+    print(f"miau, soy un gato {self.color}")
+
+atigrado=gato()
+atrigrado.color="cafe"
+atigrado.hablar()
+
+
+gris=gato()
+gris.color="gris"
+gris.hablar()
+
